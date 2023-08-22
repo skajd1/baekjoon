239493766009c6730c89d19810bf7dc03cfda6d3
skajd1 <86655177+skajd1@users.noreply.github.com>
2023-08-22 17:15:42 +0900

    [Gold IV] Title: 감시, Time: 436 ms, Memory: 245540 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,36 +8,32 @@ public class Main {
 	static int m;
 	static int[][] Room;
 	static int[][] delta = {{-1,0},{0,1},{1,0},{0,-1}};
-	static int[][] direction1 = {{0},{1},{2},{3}};
-	static int[][] direction2 = {{0,2},{1,3}};
-	static int[][] direction3 = {{0,1},{1,2},{2,3},{3,0}};
-	static int[][] direction4 = {{0,1,2},{1,2,3},{2,3,0},{3,0,1}};
-	static int[][] direction5 = {{0,1,2,3}};
+	static int[][][] direction = {
+		{{0},{1},{2},{3}},
+		{{0,2},{1,3}},
+		{{0,1},{1,2},{2,3},{3,0}},
+		{{0,1,2},{1,2,3},{2,3,0},{3,0,1}},
+		{{0,1,2,3}}
+	};
 	static int ans;
 
 	static int getSagak(int[][] room) {
 		int tmp = 0;
 		for(int i = 0 ; i < n ; i++) {
 			for(int j = 0 ; j < m ; j ++) {
-				if(room[i][j] == 0) {
-					tmp++;
-				}
+				if(room[i][j] == 0) tmp++;
 			}
 		}
 		return tmp;
 	}
 	static void changeRoom(int[] dir, int x, int y, int[][] room) {
-		int changed = 0;
 		for(int d : dir) {
 			int cnt = 1;
 			while(true) {
 				int nx = x + delta[d][0]*cnt;
 				int ny = y + delta[d][1]*cnt;
 				if(0<=nx && nx < n && 0<=ny && ny < m && (room[nx][ny] != 6 )) {
-					if(room[nx][ny] == 0) {
-						changed += 1;
-						room[nx][ny] = -1;	
-					}
+					if(room[nx][ny] == 0) room[nx][ny] = -1;	
 					cnt += 1;
 				}
 				else break;
@@ -62,39 +57,12 @@ public class Main {
 			return;
 		}
 		int[][] room_copy = new int[n][m];
-		
-		if(room[x][y] == 1) {
-			for(int i = 0 ; i < 4 ; i ++) {
-				init(room_copy, room);
-				changeRoom(direction1[i],x,y,room_copy);
+		if(room[x][y] >= 1 && room[x][y] <= 5) {
+			for(int[] d : direction[room[x][y]-1]) {
+				init(room_copy,room);
+				changeRoom(d,x,y,room_copy);
 				dfs(x,y+1,room_copy);
 			}
-		}
-		else if(room[x][y] == 2) {
-			for(int i = 0 ; i < 2 ; i ++) {
-				init(room_copy, room);
-				changeRoom(direction2[i],x,y,room_copy);
-				dfs(x,y+1,room_copy);
-			}			
-		}
-		else if(room[x][y] == 3) {
-			for(int i = 0 ; i < 4 ; i ++) {
-				init(room_copy, room);
-				changeRoom(direction3[i],x,y,room_copy);
-				dfs(x,y+1,room_copy);
-			}
-		}
-		else if(room[x][y] == 4) {
-			for(int i = 0 ; i < 4 ; i ++) {
-				init(room_copy, room);
-				changeRoom(direction4[i],x,y,room_copy);
-				dfs(x,y+1,room_copy);
-			}
-		}
-		else if(room[x][y] == 5) {
-			init(room_copy, room);
-			changeRoom(direction5[0],x,y,room_copy);
-			dfs(x,y+1,room_copy);
 		}
 		else dfs(x,y+1,room);
 	}
@@ -107,12 +75,11 @@ public class Main {
 		Room = new int[n][m];
 		for (int i = 0 ; i < n ; i ++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0 ; j < m ; j ++) {
+			for(int j = 0 ; j < m ; j ++) 
 				Room[i][j] = Integer.parseInt(st.nextToken()); 
-			}
 		}
 		ans = n*m;
 		dfs(0,0,Room);
 		System.out.println(ans);
-    }
+	}
 }
