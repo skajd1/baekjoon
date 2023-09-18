c220463e8c348701b19d4d12e2d7f79df5fa52f5
skajd1 <86655177+skajd1@users.noreply.github.com>
2023-09-18 13:09:23 +0900

    [Gold V] Title: 상어 초등학교, Time: 200 ms, Memory: 116420 KB -BaekjoonHub

--- 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
+++ 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
@@ -8,10 +8,7 @@ pref = [[] for _ in range((n**2) + 1)]
 for x in [list(map(int, input().split())) for _ in range(n**2)] : 
     order.append(x[0])
     pref[x[0]] = x[1:]
-    
-# 1. 비어있는 칸 중에서 좋아하는 학생이 인접한 칸에 가장 많은 칸으로 자리를 정한다.
-# 2. 1을 만족하는 칸이 여러 개이면, 인접한 칸 중에서 비어있는 칸이 가장 많은 칸으로 자리를 정한다.
-# 3. 2를 만족하는 칸도 여러 개인 경우에는 행의 번호가 가장 작은 칸으로, 그러한 칸도 여러 개이면 열의 번호가 가장 작은 칸으로 자리를 정한다.
+
 def setSeat():
     for o in order:
         # 초기 자리 설정 전부 다 빈칸이면 가장 왼쪽 위인 2,2에 앉힘
@@ -46,11 +43,8 @@ def setSeat():
                 elif prefNum[i][j] == maxPref:
                     if nullNum[i][j] > nullNum[maxPrefidx[0]][maxPrefidx[1]]:
                         maxPrefidx = [i,j]
-        # maxPref가 업데이트 안됐을 때 그대로 0,0에 업데이트 하는 경우 고쳐야함
-
 
         board[maxPrefidx[0]][maxPrefidx[1]] = o
-
 
 def getScore() :
     scores = [0,1,10,100,1000]
@@ -68,5 +62,4 @@ def getScore() :
     return ans
 
 setSeat()
-#board = [[6,7,4],[8,5,2],[9,1,3]]
 print(getScore())
