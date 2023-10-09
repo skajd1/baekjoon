16c79fe7a73a140edc9efafc97fdc735e76a3c03
skajd1 <86655177+skajd1@users.noreply.github.com>
2023-10-09 23:37:29 +0900

    [Gold IV] Title: 키 순서, Time: 1516 ms, Memory: 49772 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n,m;
+	static LinkedList<Integer>[] indegree;
+	static LinkedList<Integer>[] outdegree;
+	static ArrayDeque<Integer> q = new ArrayDeque<>();
+	static int func(int node) {
+		int result = 0;
+		boolean[] visited = new boolean[n+1];
+		q.add(node);
+		visited[node] = true;
+		while(!q.isEmpty()) {
+			int now = q.pollFirst();
+			for(int in : indegree[now]) {
+				if(!visited[in]) {
+					visited[in] = true;
+					q.addLast(in);
+					result += 1;
+				}
+			}
+		}
+		q.add(node);
+		visited[node] = true;
+		visited = new boolean[n+1];
+		while(!q.isEmpty()) {
+			int now = q.pollFirst();
+			for(int out : outdegree[now]) {
+				if(!visited[out]) {
+					visited[out] = true;
+					q.addLast(out);
+					result += 1;
+				}
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        indegree = new LinkedList[n+1];
+        outdegree = new LinkedList[n+1];
+        for(int i = 0 ; i <= n ; i++) {
+            indegree[i] = new LinkedList<>();
+            outdegree[i] = new LinkedList<>();
+        }
+        int ans = 0;
+        for(int i = 0 ; i < m ; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            indegree[b].add(a);
+            outdegree[a].add(b);
+        }
+        for(int i = 1 ; i <= n ; i++) {
+            if(func(i) == n-1) ans += 1;
+        }
+        System.out.println(ans);
+		
+	}
+}
