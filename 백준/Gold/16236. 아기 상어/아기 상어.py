4ade16570ee04c902560dcdb74f967289d004933
skajd1 <86655177+skajd1@users.noreply.github.com>
2023-12-06 21:07:12 +0900

    [Gold III] Title: 아기 상어, Time: 188 ms, Memory: 114276 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,49 +1,65 @@
-N = int(input())
-space = []
-curSize = 2
+from collections import deque
 dx = [-1,0,0,1]
 dy = [0,-1,1,0]
+n = int(input())
+board = [list(map(int, input().split())) for _ in range(n)]
+t = 0
 
-for n in range(N) :
-    line = list(map(int,input().split()))
-    space.append(line)
-    for l in range(len(line)) :
-        if space[n][l] == 9 :
-            space[n][l] = 0
-            cur_x, cur_y = n, l
+# 현재 상어 크기와 같은 수 만큼의 물고기를 먹으면 상어 크기가 1 증가한다.
+shark_size = 2
+size_cnt = 0
 
-def bfs(x,y) :
-    global curSize
-    visited = [[0] * N for _ in range(N)]
-    distance = [[0] * N for _ in range(N)]
-    que = []
-    que.append([x,y])
-    arr = []
-    while que :
-        x,y = que.pop(0)
-        for i in range(4) :
+sharkX, sharkY = 0, 0
+for i in range(n):
+    for j in range(n):
+        if board[i][j] == 9:
+            sharkX, sharkY = i, j
+            board[i][j] = 0
+
+def eatFish():
+    global size_cnt, shark_size
+    size_cnt += 1
+    if shark_size == size_cnt :
+        shark_size += 1
+        size_cnt = 0
+
+def findNearestFish():
+    global t, board, sharkX, sharkY
+    
+    # bfs로 현재 상어 위치로부터 먹을 수 있는 가장 가까운 물고기.
+    # 먹을 수 있는 물고기가 없으면 -1 리턴
+    # 있으면 그 물고기 까지 가는 시간 리턴
+    # 현재 상어 크기 미만의 물고기만 먹을 수 있음.
+    # 빈칸과 현재 상어 크기 이하의 물고기 칸만 지나갈 수 있음
+    visited = [[0] * n for _ in range(n)]
+    q = deque()
+    q.append((sharkX, sharkY,0))
+    visited[sharkX][sharkY] = 1
+    candidate = []
+    while q:
+        x,y,time = q.popleft()
+        for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < N :
-                if visited[nx][ny] == 0 and space[nx][ny] <= curSize :
-                    visited[nx][ny] = 1
-                    distance[nx][ny] = distance[x][y] + 1
-                    que.append([nx,ny])
-                    if space[nx][ny] != 0 and space[nx][ny] < curSize :
-                        arr.append([nx,ny,distance[nx][ny]])
-    arr.sort(key=lambda x : (x[2],x[0],x[1]))
-    return arr
+            if 0<=nx<n and 0<=ny < n and not visited[nx][ny] :
+                visited[nx][ny] = 1
+                if board[nx][ny] > shark_size:
+                    continue
+                elif board[nx][ny] < shark_size and board[nx][ny] != 0:
+                    candidate.append((nx,ny,time+1))
+                elif board[nx][ny] == shark_size or board[nx][ny] == 0 :
+                    q.append((nx,ny,time+1))
+    if candidate :
+        candidate.sort(key=lambda x:(x[2],x[0],x[1]))
+        board[candidate[0][0]][candidate[0][1]] = 0
+        sharkX = candidate[0][0]
+        sharkY = candidate[0][1]
+        eatFish()
+        return candidate[0][2]
+    return -1
+                
 
-answer,cnt = 0,0
-while True :
-    fishList = bfs(cur_x,cur_y)
-    if len(fishList) == 0 :
-        print(answer)
-        break
-    cur_x,cur_y,dist = fishList[0]
-    cnt += 1
-    if cnt == curSize :
-        curSize += 1
-        cnt = 0
-    answer += dist
-    space[cur_x][cur_y] = 0
+while (x :=findNearestFish()) != -1:
+    t += x
+    
+print(t)
